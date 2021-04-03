0c5a0ad9fd8ea5e1d3b1089699a9a8b222c0ab10
Zeeshan Tnveer <z.tanveer@alignpx.com>
2021-04-03 11:44:22 +0500

    Resume data Updated

---
+++ src/resumeData.js
@@ -0,0 +1,91 @@
+let resumeData = {
+    "imagebaseurl": "https://rbhatia46.github.io/",
+    "name": "Zeeshan Tanveer",
+    "role": "Software Engineer",
+    "linkedinId": "My LinkedIn ID",
+    "skypeid": "Your Skype ID",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/zeeshan-tanveer/",
+            "className": "fa fa-linkedin"
+        }
+    ],
+    "aboutme": "I am currently a senior software engineer at AlignPx Technologies Pvt limited. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address": "Pakistan",
+    "website": "https://rbhatia46.github.io",
+    "education": [
+        {
+            "UniversityName": "Virtual University of Pakistan",
+            "specialization": "Computer Science",
+            "MonthOfPassing": "Aug",
+            "YearOfPassing": "2018",
+        }
+    ],
+    "work": [
+        {
+            "CompanyName": "AlignPx Technologies",
+            "specialization": "PHP Laravel Framework",
+            "MonthOfLeaving": "Mar",
+            "YearOfLeaving": "2019",
+        }
+    ],
+    "skillsDescription": "",
+    "skills": [
+        {
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "JavaScript"
+        },
+        {
+            "skillname": "React JS"
+        },
+        {
+            "skillname": "PHP"
+        },
+        {
+            "skillname": "Object Oriented Programming"
+        },
+        {
+            "skillname": "MySql"
+        },
+        {
+            "skillname": "Laravel"
+        },
+        {
+            "skillname": "Codeigniter"
+        },
+        {
+            "skillname": "API Integration"
+        },
+        {
+            "skillname": "API Documentation Development"
+        },
+        {
+            "skillname": "REST"
+        },
+        {
+            "skillname": "GIT"
+        },
+        {
+            "skillname": "CPanel"
+        },
+        {
+            "skillname": "AWS"
+        }
+    ],
+    "portfolio": [
+        {
+            "name": "FDO",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/banner-main.jpg"
+        }
+    ]
+}
+
+export default resumeData
